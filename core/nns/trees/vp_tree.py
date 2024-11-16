950ea5e63e9af042a33d6f50c59435320eaf0b1d
LJ S.A <lvjhn.d@gmail.com>
2024-11-16 18:22:12 +0800

    prepared template for VPTree

--- core/nns/trees/vp_tree.py
+++ core/nns/trees/vp_tree.py
@@ -1,6 +1,25 @@
 
 """ 
     VANTAGE POINT TREE 
+    -------------------------------------------------------------------------------------
+    Modified version of: 
+        https://github.com/RickardSjogren/vptree/blob/master/vptree.py
+    -------------------------------------------------------------------------------------
 """ 
 from ..indexer import Indexer
 
+class VPTree(Indexer): 
+    def __init__(self, *args, **kwargs): 
+        Indexer.__init__(self, *args, **kwargs) 
+
+    """ 
+        TREE CONSTRUCTION 
+    """ 
+    def build_indexer(self, points): 
+        pass  
+
+    """ 
+        TREE QUERY
+    """ 
+    def query(self, target, k): 
+        pass
